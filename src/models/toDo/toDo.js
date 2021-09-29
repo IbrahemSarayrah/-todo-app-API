49f9c7549f27a11a3a9cba3a545905dc202d4c97
IbrahemSarayrah <ibrahem.sarayrah@gmail.com>
2021-09-29 13:35:14 +0300

    update

--- src/models/toDo/toDo.js
+++ src/models/toDo/toDo.js
@@ -4,7 +4,7 @@ const toDoModel = (sequelize, DataTypes) => sequelize.define('TODO', {
     toDoItem: { type: DataTypes.STRING, required: true },
     assignedTo: { type: DataTypes.STRING, required: true },
     difficulty: { type: DataTypes.INTEGER, required: true },
-    id: { type: DataTypes.STRING, required: true },
+    toDoid: { type: DataTypes.STRING, required: true },
     complete: { type: DataTypes.BOOLEAN, required: true },
 });
 
