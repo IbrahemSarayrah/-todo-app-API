e7cf22ed82b2a27f1cc391ee0bd242d4fbeed14e
IbrahemSarayrah <ibrahem.sarayrah@gmail.com>
2021-09-29 11:46:16 +0300

    backend

--- src/models/toDo/toDo.js
+++ src/models/toDo/toDo.js
@@ -4,7 +4,6 @@ const toDoModel = (sequelize, DataTypes) => sequelize.define('TODO', {
     toDoItem: { type: DataTypes.STRING, required: true },
     assignedTo: { type: DataTypes.STRING, required: true },
     difficulty: { type: DataTypes.INTEGER, required: true },
-    userId:{type: DataTypes.INTEGER}
 });
 
 module.exports = toDoModel;
